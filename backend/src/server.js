0430d852da6e76968d249b7252ad8b144f40e553
Juan Ignacio Mercado <set.juanidev@gmail.com>
2026-09-15 13:48:02 +0200

    Setup inicial de frontend (React+Vite) y backend (Express)

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('Cortify API funcionando.')
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running in PORT ${PORT}`));
